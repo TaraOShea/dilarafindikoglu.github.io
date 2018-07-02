36d03588cb3202a541ba6b424d90f55f4b02a2ed
TaraWaste <tara@wastecreative.com>
2018-07-02 16:47:09 +0100

    back btn

--- js/helpers/productFlick.js
+++ js/helpers/productFlick.js
@@ -1,14 +1,16 @@
 gs.flick = function() {
-	gs.carousel = $('.product-images').flickity({
-		cellSelector: '.product-image', 
-		contain: true,
-		adaptiveHeight: true
-	});
-	if($(window).width() <= 920) {
-		gs.carousel.flickity();
-		gs.flickEnabled = true;
-	} else {
-		gs.carousel.flickity('destroy');
-		gs.flickEnabled = false;
-	}
+    if($('.product-images')){
+        gs.carousel = $('.product-images').flickity({
+            cellSelector: '.product-image', 
+            contain: true,
+            adaptiveHeight: true
+        });
+        if($(window).width() <= 920) {
+            gs.carousel.flickity();
+            gs.flickEnabled = true;
+        } else {
+            gs.carousel.flickity('destroy');
+            gs.flickEnabled = false;
+        }
+    }   
 }
