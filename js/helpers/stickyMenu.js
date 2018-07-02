4d7e20668046f322eef4f625795792bbbd6e69ab
TaraWaste <tara@wastecreative.com>
2018-07-02 16:20:04 +0100

    Product page update

--- js/helpers/stickyMenu.js
+++ js/helpers/stickyMenu.js
@@ -1,12 +1,14 @@
 //http://jsfiddle.net/woun56vk/203/
 gs.logoScrollResize = function(){
     gs.logo = document.getElementById("logo");
-    gs.header = document.getElementById("header")
-    gs.logoW = gs.logo.offsetWidth;
+    gs.logotxt = document.getElementById("logo-text");
+    gs.logosvg = document.getElementById("logo-svg");
+    gs.header = document.getElementById("header");
+    gs.logoW = Math.floor(gs.logo.offsetWidth);
     gs.stick = gs.logo.offsetHeight + gs.logo.offsetTop;
-    gs.maxScrollDistance = gs.header.offsetHeight;
+    gs.maxScrollDistance = gs.header.offsetHeight/2;
     
-    gs.widthAtMax = 120;
+    gs.widthAtMax = 250;
     gs.widthDiff = gs.logoW - gs.widthAtMax;
     gs.pixelsPerScroll = (gs.widthDiff / gs.maxScrollDistance);
 
@@ -19,14 +21,24 @@ gs.stickyMenu = function() {
         var scrollTopPos = Math.min(window.pageYOffset, gs.maxScrollDistance);
         var scrollChangePx =  Math.floor(scrollTopPos * gs.pixelsPerScroll);
         var zoomedWidth = gs.logoW - scrollChangePx;
-        logo.style.maxWidth = zoomedWidth +"px";
+        // var zoomedWidth = gs.logoW - (scrollChangePx*2);
+        // if(zoomedWidth >= gs.widthAtMax) {
+            gs.logo.style.maxWidth = zoomedWidth +"px";
+        // }
 
         if (window.pageYOffset <= gs.maxScrollDistance && (window.innerHeight + window.scrollY) <= (document.body.offsetHeight) - document.getElementById("footer").offsetHeight) {
-            content.style.paddingTop = gs.maxScrollDistance  - window.pageYOffset +"px";
-        } 
+            content.style.paddingTop = (gs.maxScrollDistance*2)  - window.pageYOffset +"px";
+        }
+
+        if(gs.logo.offsetWidth <= gs.widthAtMax + 10) {
+            gs.logosvg.style.marginTop = "-130px";
+            gs.logotxt.style.display = "none";
+        } else {
+            gs.logosvg.style.marginTop = "0px";
+            gs.logotxt.style.display = "block"; 
+        }
     } else {
-        console.log("hi");
-        logo.style.maxWidth = gs.widthAtMax +"px"; 
+        gs.logo.style.maxWidth = gs.widthAtMax +"px"; 
         content.style.paddingTop = gs.header.offsetHeight +"px"; 
     }
 }
